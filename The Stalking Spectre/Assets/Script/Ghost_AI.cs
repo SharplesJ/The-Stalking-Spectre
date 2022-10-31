64dc1b286917e1596f8870c722deb61babb897bc
Jake Sharples <s012349l@student.staffs.ac.uk>
2022-10-31 14:50:21 +0000

    ghost ai

---
+++ The Stalking Spectre/Assets/Script/Ghost_AI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost_AI : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get player's position
+        GameObject player = GameObject.Find("Player");
+        Transform playerTransform = player.transform;
+        Vector3 playerPosition = playerTransform.position;
+
+
+        //get ghosts position
+        GameObject ghost = GameObject.Find("Ghost");
+        Transform ghostTransform = ghost.transform;
+        Vector3 ghostPosition = ghostTransform.position;
+
+        if ((playerPosition.x < ghostPosition.x) & (playerPosition.z < ghostPosition.z))
+        {
+            transform.position = transform.position + new Vector3(-movementSpeed * Time.deltaTime, 0, -movementSpeed * Time.deltaTime);
+        }
+        else if ((playerPosition.x > ghostPosition.x) & (playerPosition.z < ghostPosition.z))
+        {
+            transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, -movementSpeed * Time.deltaTime);
+        }
+        else if ((playerPosition.x < ghostPosition.x) & (playerPosition.z > ghostPosition.z))
+        {
+            transform.position = transform.position + new Vector3(-movementSpeed * Time.deltaTime, 0, movementSpeed * Time.deltaTime);
+        }
+        else if ((playerPosition.x > ghostPosition.x) & (playerPosition.z > ghostPosition.z))
+        {
+            transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, movementSpeed * Time.deltaTime);
+        }
+    }
+}
